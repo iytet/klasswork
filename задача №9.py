1fe143fcb209fc95abf3b6bad454c141f7154ada
iytet <163995480+iytet@users.noreply.github.com>
2024-03-19 14:31:59 +0300

    задачи с 3 по 8,10

---
+++ задача №9.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+
+#Задача№9
+x=int (input("Введите число:"))
+a=int (input("Введите число:"))
+if x<1:
+   y=a ** 2 + x ** 2
+   print (y)
+else:
+    y=a ** 2 - x ** 2
+    print (y)
+    
