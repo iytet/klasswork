ad815e04bb25f43bf7738074fc587bcb9e8ea7a8
iytet <163995480+iytet@users.noreply.github.com>
2024-03-30 13:43:24 +0300

    Практическая работа продолжение от 27.02.24 г.

--- main.py
+++ main.py
@@ -35,6 +35,44 @@ K = ((h ** 2) + ((g - i) /2) ** 2) ** 0.5
 L = i + g + 2 * K
 print ("L = " , L)
 
+#Задача №11
+v = int(input("Введите v = "))
+m = int(input("Введите m = "))
+Po = m/v
+print ("Po = " , Po)
 
+#Задача №12
+x = int(input("Введите x = "))
+y = int(input("Введите y = "))
+z = x + y
+q = x - y
+t = x * y
+u = x / y
+print ("z = " , z)
+print ("q = " , q)
+print ("t = " , t)
+print ("u = " , u)
 
+#Задача №13
+x1 = int(input("Введите x1 = "))
+x2 = int(input("Введите x2 = "))
+N = (x1 + x2) /2 
+J = (x1 * x2) ** 0.5
+print ("N = " , N)
+print ("J = " , J)
+
+#Задача №14
+k = int(input("Введите k = "))
+Q = int(input("Введите Q = "))
+Pl = k/Q
+print ("Pl = " , Pl)
+
+#Задача №15
+a1 = int(input("Введите a1 = "))
+b1 = int(input("Введите b1 = "))
+c1 = int(input("Введите c1 = "))
+S1 = 2 *(a1 * b1 + a1 * c1 + b1 * c1)
+V1 = a1 * b1 * c1
+print ("S1 = " , S1)
+print ("V1 = " , V1)
 
